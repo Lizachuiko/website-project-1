8f2c8f678041284b313271155d580f6bd8247ae4
Liza Chuiko <lozarti@mail.ru>
2017-11-02 14:50:56 +0300

    new js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,35 @@
 $(document).ready(function() {
     $('.team__ACCORDEON__member').click(
-     function() {
-      $(this).addClass('team__ACCORDEON__member-hidden');
-     },
-     function() {
-      $(this).removeClass('team__ACCORDEON__member-hidden');
-     }
-    )})
+        function() {
+            $(this).toggleClass( 'team__ACCORDEON__member-hidden' );
+        })
+})
+
+$(document).ready(function() {
+    $('.accordion').asAccordion({
+      namespace: 'accordion',
+      // accordion theme. WIP
+      skin: null,
+  
+      // breakpoint for mobile devices. WIP
+      mobileBreakpoint: 768,
+  
+      // initial index panel
+      initialIndex: 0,
+  
+      // CSS3 easing effects.
+      easing: 'ease-in-out',
+  
+      // animation speed.
+      speed: 500,
+  
+      // vertical or horizontal
+      direction: 'horizontal',
+  
+      // jQuery mouse events. click, mousehover, etc.
+      event: 'click',
+  
+      // multiple instance
+      multiple: false
+    });
+  });
