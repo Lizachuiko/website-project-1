9eb363aa817cae8549070a72db28cf7f765a6cf0
Liza Chu <lozarti@mail.ru>
2017-11-15 20:18:02 +0300

    add acco js for team

--- js/main.js
+++ js/main.js
@@ -1,43 +1,51 @@
 $(document).ready(function() {
-    $('.team__ACCORDEON__member').click(
-        function() {
-            $(this).toggleClass( 'team__ACCORDEON__member-hidden' );
-        })
-})
 
-$(document).ready(function() {
-    $('.accordion').asAccordion({
-      namespace: 'accordion',
-      // accordion theme. WIP
-      skin: null,
+    $('.team-acco__item').click(
+        function() {
+            $(this).toggleClass('team-acco__item__hidden');
+        })
+});
+
+// $(document).ready(function() {
+//     $('.accordion').asAccordion({
+//       namespace: 'accordion',
+//       // accordion theme. WIP
+//       skin: null,
   
-      // breakpoint for mobile devices. WIP
-      mobileBreakpoint: 768,
+//       // breakpoint for mobile devices. WIP
+//       mobileBreakpoint: 768,
   
-      // initial index panel
-      initialIndex: 0,
+//       // initial index panel
+//       initialIndex: 0,
   
-      // CSS3 easing effects.
-      easing: 'ease-in-out',
+//       // CSS3 easing effects.
+//       easing: 'ease-in-out',
   
-      // animation speed.
-      speed: 500,
+//       // animation speed.
+//       speed: 500,
   
-      // vertical or horizontal
-      direction: 'horizontal',
+//       // vertical or horizontal
+//       direction: 'horizontal',
   
-      // jQuery mouse events. click, mousehover, etc.
-      event: 'click',
+//       // jQuery mouse events. click, mousehover, etc.
+//       event: 'click',
   
-      // multiple instance
-      multiple: false
-    });
-  });
+//       // multiple instance
+//       multiple: false
+//     });
+//   });
 
 
   var hero = document.getElementById('hero')
   var hamburger = document.getElementById('hamburger')
+  var cross = document.getElementById('cross')
 
   hamburger.addEventListener('click', function() {
       hero.classList.add('full-menu')
-  })
+
+  });
+
+ cross.addEventListener('click', function() {
+    hero.classList.remove('full-menu')
+
+});
