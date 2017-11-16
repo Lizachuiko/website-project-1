8c852b97af73c31f2d91441aaec19d0d0ae4a73f
Liza Chu <lozarti@mail.ru>
2017-11-17 00:19:16 +0300

    js acco

--- js/main.js
+++ js/main.js
@@ -7,35 +7,29 @@ $(document).ready(function() {
         })
 });
 
-// $(document).ready(function() {
-//     $('.accordion').asAccordion({
-//       namespace: 'accordion',
-//       // accordion theme. WIP
-//       skin: null,
-  
-//       // breakpoint for mobile devices. WIP
-//       mobileBreakpoint: 768,
-  
-//       // initial index panel
-//       initialIndex: 0,
-  
-//       // CSS3 easing effects.
-//       easing: 'ease-in-out',
-  
-//       // animation speed.
-//       speed: 500,
-  
-//       // vertical or horizontal
-//       direction: 'horizontal',
-  
-//       // jQuery mouse events. click, mousehover, etc.
-//       event: 'click',
-  
-//       // multiple instance
-//       multiple: false
-//     });
-//   });
 
+// acco query
+
+// $(document).ready(function() {
+    
+//     $('.menu-acco__trigger').click( 
+//         function(e) {
+//             e.preventDefault();
+//             const item = $(e.currentTarget);
+//             $(this).toggleClass('menu-acco__trigger__hidden');
+//         });
+//         
+// });
+
+// $(document).ready(function() {
+    
+//     $('.menu-acco__trigger').click( 
+//         function(e) {
+//             e.preventDefault();
+//             // $(this).toggleClass('menu-acco__trigger__hidden');
+//             $('.menu-acco__content').slideToggle(1);
+//         })
+// });
 
   var hero = document.getElementById('hero')
   var hamburger = document.getElementById('hamburger')
@@ -63,3 +57,39 @@ mobileCross.addEventListener('click', function(e) {
     e.preventDefault();
     review.classList.remove('review-mobile-hidden')
 });
+
+
+$(function() {
+    $('.acco__trigger').on('click', e => { //menu-acco__trigger
+        e.preventDefault()
+
+        const $this = $(e.currentTarget); // на что жмем
+        const container = $this.closest('.acco'); //то в чем ассордеон menu-acco
+        const item = $this.closest('.acco__item'); //menu-acco__item
+        const items = $('acco__item', container);//
+        const content = $('.acco__content', item);//menu-acco__content
+        const otherContent = $('.acco__content', container);//
+        const textBlock = $('acco__content-text', item);//menu-acco__text
+        const reqHeight = textBlock.outerHeight();//
+
+        if (!item.hasClass('active')) {
+            items.removeClass('active')
+            item.addClass('active')
+
+            otherContent.css({
+                'height':0
+            })
+
+            content.css({
+                'height': reqHeight
+            })
+
+        } else {
+            item.removeClass('active');
+            content.css({
+                'height':0
+            })
+        }
+        
+    })
+})
